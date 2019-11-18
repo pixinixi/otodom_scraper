d63e7a1d4d5422395cd6e02111ee5701e09f1fe5
Angelika <angelika.kosinska@regalix-inc.com>
2019-11-18 13:33:27 +0100

    initial commit

---
+++ otodom/items.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+from scrapy.loader.processors import TakeFirst, MapCompose
+
+
+def filter_price(value):
+    # \xa0 - space
+    value = value.replace('zł', "").replace("\xa0", "").replace(",", ".").replace(".00", "") \
+        .replace("\n", "").replace(" ", "")
+    try:
+        float(value)
+        return value
+    except ValueError:
+        return None
+
+
+def filter_powierzchnia(value):
+    value = value.replace(' m²', "").replace("\xa0", "").replace(",", ".").replace(".00", "") \
+        .replace("\n", "").replace(" ", "")
+    try:
+        float(value)
+        return value
+    except ValueError:
+        return None
+
+
+class OtodomItem(scrapy.Item):
+    tytul = scrapy.Field(output_processor=TakeFirst())
+    cena = scrapy.Field(input_processor=MapCompose(filter_price), output_processor=TakeFirst())
+    link = scrapy.Field(output_processor=TakeFirst())
+    address = scrapy.Field(output_processor=TakeFirst())
+    lat = scrapy.Field(output_processor=TakeFirst())
+    lon = scrapy.Field(output_processor=TakeFirst())
+    powierzchnia = scrapy.Field(input_processor=MapCompose(filter_powierzchnia), output_processor=TakeFirst())
+    rodzaj_zabudowy = scrapy.Field(output_processor=TakeFirst())
+    czynsz = scrapy.Field(input_processor=MapCompose(filter_price), output_processor=TakeFirst())
+    okna = scrapy.Field(output_processor=TakeFirst())
+    forma_wlasnosci = scrapy.Field(output_processor=TakeFirst())
+    liczba_pokoi = scrapy.Field(output_processor=TakeFirst())
+    pietro = scrapy.Field(output_processor=TakeFirst())
+    ogrzewanie = scrapy.Field(output_processor=TakeFirst())
+    rynek = scrapy.Field(output_processor=TakeFirst())
+    liczba_pieter = scrapy.Field(output_processor=TakeFirst())
+    stan_wykonczenia = scrapy.Field(output_processor=TakeFirst())
+    opis = scrapy.Field(output_processor=TakeFirst())
+    rok_budowy = scrapy.Field(output_processor=TakeFirst())
+    material_budynku = scrapy.Field(output_processor=TakeFirst())
+
+    balkon = scrapy.Field(output_processor=TakeFirst())
+    drzwi_okna_anty = scrapy.Field(output_processor=TakeFirst())
+    pom_uzytkowe = scrapy.Field(output_processor=TakeFirst())
+    garaz_miejsce_park = scrapy.Field(output_processor=TakeFirst())
+    piwnica = scrapy.Field(output_processor=TakeFirst())
+    ogrodek = scrapy.Field(output_processor=TakeFirst())
+    taras = scrapy.Field(output_processor=TakeFirst())
+    winda = scrapy.Field(output_processor=TakeFirst())
+    dwupoziomowe = scrapy.Field(output_processor=TakeFirst())
+    oddzielna_kuchnia = scrapy.Field(output_processor=TakeFirst())
+    klimatyzacja = scrapy.Field(output_processor=TakeFirst())
+    system_alarmowy = scrapy.Field(output_processor=TakeFirst())
+    monitoring_ochrona = scrapy.Field(output_processor=TakeFirst())
+    teren_zamkniety = scrapy.Field(output_processor=TakeFirst())
+
+    data_pobrania = scrapy.Field(output_processor=TakeFirst())
---
+++ otodom/spiders/otodomSpider.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+import json
+from datetime import datetime
+
+import scrapy
+from scrapy import Request
+from scrapy.loader import ItemLoader
+from scrapy.utils.markup import remove_tags
+
+from otodom.items import OtodomItem
+
+
+class OtodomMieszkaniaSpider(scrapy.Spider):
+    name = 'otodomSpider'
+
+    def __init__(self, city=None, *args, **kwargs):
+        super(OtodomMieszkaniaSpider, self).__init__(*args, **kwargs)
+        self.start_urls = [f'https://www.otodom.pl/sprzedaz/mieszkanie/{city}/']
+
+    def parse(self, response):
+        for offer in response.css("header.offer-item-header > h3"):
+            link_to_offer = offer.xpath('.//a/@href').get()
+            request_to_offer = Request(link_to_offer, callback=self.get_offer_details)
+            yield request_to_offer
+
+        next_page_link = response.xpath('//li[@class="pager-next"]/a/@href')
+        if next_page_link:
+            yield response.follow(next_page_link.extract_first(), callback=self.parse)
+
+    def get_offer_details(self, response):
+        # find script with lon and lat
+        offer_item = ItemLoader(item=OtodomItem(), selector=response)
+
+        offer_item.add_value('link', response.url)
+        offer_item.add_xpath('tytul', "//h1[@class='css-18igut2']/text()")
+        offer_item.add_xpath('cena', '//div[@class="css-1vr19r7"]/text()')
+
+        for script in response.xpath("//script[@type='application/ld+json']"):
+            # there is usually a few <scripts> of this type in different order
+            # and Im only looking for the one with the coordinates
+            if 'GeoCoordinates' in script.get():
+                json_offer = json.loads(remove_tags(script.extract()))
+
+                lat = json_offer['@graph'][0]['geo']['latitude']
+                lon = json_offer['@graph'][0]['geo']['longitude']
+                address = json_offer['@graph'][0]['address']['streetAddress']
+                description = json_offer['@graph'][0]['description']
+
+                offer_item.add_value('lat', lat)
+                offer_item.add_value('lon', lon)
+                offer_item.add_value('address', address)
+                offer_item.add_value('opis', description)
+
+        offer_item.add_value('data_pobrania', str(datetime.now()))
+        offer_item.add_xpath('powierzchnia', '//li[contains(normalize-space(.), "Powierzchnia:")]/strong/text()')
+        offer_item.add_xpath('rodzaj_zabudowy', '//li[contains(normalize-space(.), "Rodzaj zabudowy:")]/strong/text()')
+        offer_item.add_xpath('okna', '//li[contains(normalize-space(.), "Okna:")]/strong/text()')
+        offer_item.add_xpath('forma_wlasnosci', '//li[contains(normalize-space(.), "Forma własności:")]/strong/text()')
+        offer_item.add_xpath('liczba_pokoi', '//li[contains(normalize-space(.), "Liczba pokoi:")]/strong/text()')
+        offer_item.add_xpath('pietro', '//li[contains(normalize-space(.), "Piętro:")]/strong/text()')
+        offer_item.add_xpath('ogrzewanie', '//li[contains(normalize-space(.), "Ogrzewanie:")]/strong/text()')
+        offer_item.add_xpath('rynek', '//li[contains(normalize-space(.), "Rynek:")]/strong/text()')
+        offer_item.add_xpath('liczba_pieter', '//li[contains(normalize-space(.), "Liczba pięter:")]/strong/text()')
+        offer_item.add_xpath('stan_wykonczenia',
+                             '//li[contains(normalize-space(.), "Stan wykończenia:")]/strong/text()')
+        offer_item.add_xpath('czynsz',
+                             '//li[contains(normalize-space(.), "Czynsz:")]/strong/text()')
+        offer_item.add_xpath('forma_wlasnosci',
+                             '//li[contains(normalize-space(.), "Forma własności:")]/strong/text()')
+        offer_item.add_xpath('rok_budowy',
+                             '//li[contains(normalize-space(.), "Rok budowy:")]/strong/text()')
+        offer_item.add_xpath('material_budynku',
+                             '//li[contains(normalize-space(.), "Materiał budynku:")]/strong/text()')
+
+        extras = {'drzwi_okna_anty': 'drzwi / okna antywłamaniowe',
+                  'pom_uzytkowe': 'pom. użytkowe',
+                  'garaz_miejsce_park': 'garaż/miejsce parkingowe',
+                  'piwnica': 'piwnica',
+                  'ogrodek': 'ogródek',
+                  'taras': 'taras',
+                  'winda': 'winda',
+                  'dwupoziomowe': 'dwupoziomowe',
+                  'oddzielna_kuchnia': 'oddzielna kuchnia',
+                  'klimatyzacja': 'klimatyzacja',
+                  'system_alarmowy': 'system alarmowy',
+                  'monitoring_ochrona': 'monitoring / ochrona',
+                  'balkon': 'balkon',
+                  'teren_zamkniety': 'teren zamknięty'}
+
+        extras_list = response.xpath('//div[@class="css-1bpegon"]/ul/li/text()').extract()
+        for key, value in extras.items():
+            if value in extras_list:
+                offer_item.add_value(key, 1)
+            else:
+                offer_item.add_value(key, 0)
+
+        yield offer_item.load_item()
